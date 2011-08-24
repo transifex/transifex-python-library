001b239ff4a53518dc0a5c240767c81cb01474b8
Apostolos Bessas <mpessas@indifex.com>
2011-08-24 13:43:05 +0300

    Restructure HTTP exception hierarchy.
    Rename base class and add exception for remote server errors (5xx).

--- txlib/http/exceptions.py
+++ txlib/http/exceptions.py
@@ -4,37 +4,41 @@ HTTP related exceptions.
 """
 
 
-class RemoteServerError(Exception):
+class ServerError(Exception):
     """Base class for exceptions for problems with the remote server."""
 
     def __init__(self, *args, **kwargs):
         if 'http_code' in kwargs:
             self.http_code = kwargs.pop('http_code')
-        super(RemoteServerError, self).__init__(*args, **kwargs)
+        super(ServerError, self).__init__(*args, **kwargs)
 
 
-class RequestError(RemoteServerError):
+class RequestError(ServerError):
     """Class to indicate a generic error with the request
     to the remote server.
     """
     pass
 
 
-class NotFoundError(RemoteServerError):
+class NotFoundError(ServerError):
     """Class to indicate a resource has not been found in the remote server."""
     pass
 
 
-class AuthenticationError(RemoteServerError):
+class AuthenticationError(ServerError):
     """Class to indicate a problem with the username and/or password."""
     pass
 
 
-class AuthorizationError(RemoteServerError):
+class AuthorizationError(ServerError):
     """Class to indicate that authorization is required."""
     pass
 
 
-class UnknownError(RemoteServerError):
+class UnknownError(ServerError):
     """Class for errors which are not handled specifically."""
     pass
+
+
+class RemoteServerError(ServerError):
+    """Class for 5xx errors."""
