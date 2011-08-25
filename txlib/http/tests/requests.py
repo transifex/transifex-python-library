2ddad0fbdd785ecc6c8f4148370638cddb94a4ea
Apostolos Bessas <mpessas@indifex.com>
2011-08-25 18:35:16 +0300

    Add tests for making http requests.

--- txlib/http/tests/requests.py
+++ txlib/http/tests/requests.py
@@ -3,8 +3,12 @@
 Tests for requests.
 """
 
+from txlib.http import json
 from txlib.http.base import BaseRequest
-from txlib.http.exceptions import UnknownError, RemoteServerError
+from txlib.http.http_requests import HttpRequest
+from txlib.http.auth import AnonymousAuth, BasicAuth
+from txlib.http.exceptions import UnknownError, RemoteServerError, \
+        AuthorizationError, ConflictError, NotFoundError, RequestError
 from txlib.http.tests import unittest
 
 
@@ -98,3 +102,75 @@ class TestBaseRequest(unittest.TestCase):
         self.assertIs(b._exception_for(499), UnknownError)
         for code in (500, 501, 502, ):
             self.assertIs(b._exception_for(code), RemoteServerError)
+
+
+class TestHttpRequest(unittest.TestCase):
+    """Test the HttpRequest class.
+
+    These tests run against a local transifex installation. We assume
+    transifex listens in 127.0.0.1:8000 and there is a user `txlib` that has
+    the password `txlib`.
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        cls.hostname = 'http://127.0.0.1:8000'
+        cls.username = 'txlib'
+        cls.password = 'txlib'
+        cls.auth = BasicAuth(cls.username, cls.password)
+
+    def test_anonymous_requests(self):
+        """Test anonymous requests.
+
+        They should all fail, since transifex currently does not allow
+        anonymous access to the API.
+        """
+        auth = AnonymousAuth()
+        h = HttpRequest(self.hostname, auth=auth)
+        self.assertRaises(AuthorizationError, h.get, '/api/2/projects/')
+
+    def test_wrong_auth(self):
+        """Test response for requests with wrong authentication info."""
+        auth = BasicAuth(self.username, 'wrong')
+        h = HttpRequest(self.hostname, auth=auth)
+        self.assertRaises(AuthorizationError, h.get, '/api/2/projects/')
+
+    def test_auth(self):
+        """Test authenticated requests."""
+        path = '/api/2/projects/'
+        h = HttpRequest(self.hostname, auth=self.auth)
+        res = h.get(path)       # Succeeds!
+
+    def test_script(self):
+        h = HttpRequest(self.hostname, auth=self.auth)
+
+        # get a project that does not exist
+        self.assertRaises(NotFoundError, h.get, '/api/2/txlib/')
+
+        # create a project
+        data = json.dumps(dict(slug='txlib', name='Txlib project'))
+        path = '/api/2/projects/'
+        h.post(path, data=data)
+
+        # creating the same project results in an error raised.
+        self.assertRaises(ConflictError, h.post, path, data=data)
+
+        # Using a non-existent attribute for projects
+        data = json.dumps(dict(name='New name', anyone_submitt=True))
+        path = '/api/2/project/txlib/'
+        self.assertRaises(RequestError, h.put, path, data)
+
+        # make sure the field has the default value
+        p = h.get(path + '?details')
+        self.assertFalse(p['anyone_submit'])
+
+        # update the details of the project
+        data = json.dumps(dict(name='New name',  anyone_submit=True))
+        h.put(path, data)
+
+        # make sure the change has been saved
+        p = h.get(path + '?details')
+        self.assertTrue(p['anyone_submit'])
+
+        # delete the project
+        h.delete(path)
