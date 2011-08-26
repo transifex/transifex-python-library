463c6075148378847380a220f84ab892965bd4c4
Apostolos Bessas <mpessas@indifex.com>
2011-08-26 15:06:12 +0300

    Create registry object ot be used in the code.
    The registry object is used to fetch the specific object that provides some
    functionality.

--- txlib/registry/__init__.py
+++ txlib/registry/__init__.py
@@ -7,3 +7,6 @@ This package allows the user of the package to change the classes and functions
 used in txlib, so that he can easily change the implementations used in various
 parts.
 """
+
+from txlib.registry.registry import _Registry
+registry = _Registry()
