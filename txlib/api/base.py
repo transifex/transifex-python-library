fa70331ff604119cee1a73a876347244873894bc
Apostolos Bessas <mpessas@indifex.com>
2011-08-26 16:10:49 +0300

    Create base classes for the Tx model wrappers and their backends.

--- txlib/api/base.py
+++ txlib/api/base.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from txib.utils import _logger
+from txlib.registry import registry
 
 
 class BaseModel(object):
@@ -15,10 +16,6 @@ class BaseModel(object):
     """
 
     fields = []
-
-    _prefix = '/api/2/'
-    _path_to_collection = ''    # shouldn't start with a slash
-    _path_to_item = ''          # shouldn't start with a slash
 
     @staticmethod
     def get(self, *args, **kwargs):
@@ -37,13 +34,59 @@ class BaseModel(object):
         """Populate the isntance with the values from the server."""
         pass
 
+
+class BaseBackend(object):
+    """Base class for Tx models backend.
+
+    Each backend allows to get, create, edit and delete a resource.
+    """
+
+    _prefix = '/api/2/'
+    _path_to_collection = ''    # shouldn't start with a slash
+    _path_to_item = ''          # shouldn't start with a slash
+
+    def __init__(self):
+        """Initializer.
+
+        Setup the http handler.
+        """
+        self.http = registry.http_handler
+
+    def get(self, **kwargs):
+        """Get the resource from a remote Tx server."""
+        (path, kwargs) = self._construct_path_to_item(**kwargs)
+        return self.http.get(path)
+
+    def create(self):
+        """Create a resource in the remote Tx server."""
+        (path, kwargs) = self._construct_path_to_collection(**kwargs)
+        return self.http.post(path, kwargs)
+
+    def edit(self):
+        """Edit a resource in a remote Tx server."""
+        (path, kwargs) = self._construct_path_to_item(**kwargs)
+        return self.http.put(path, kwargs)
+
+    def delete(self):
+        """Delete a resource from a remote Tx server."""
+        (path, kwargs) = self._construct_path_to_item(**kwargs)
+        return self.http.delete(path, kwargs)
+
+    def _construct_path_to_collection(self, **kwargs):
+        """Construct the path to an actual collection."""
+        raise NotImplementedError
+
+    def _construct_path_to_item(self, **kwargs):
+        """Construct the path to an actual item."""
+        raise NotImplementedError
+
     @property
-    def path_to_collection(self):
+    def path_to_collection_template(self):
         """The URL to access the collection of the model."""
         return self._join_subpaths(self._prefix, self._path_to_collection)
 
     @property
-    def path_to_item(self):
+    def path_to_item_template(self):
         """The URL to access a specific item of the model."""
         return self._join_subpaths(self._prefix, self._path_to_item)
 
@@ -52,5 +95,4 @@ class BaseModel(object):
         well-defined URL path.
         """
         return '/'.join(args).replace('///', '/').replace('//', '/')
-
 
