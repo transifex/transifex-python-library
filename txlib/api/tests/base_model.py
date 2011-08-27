981bea629dfb4d75a376f3de0cf09561c09221ba
Apostolos Bessas <mpessas@indifex.com>
2011-08-27 18:42:38 +0300

    Tests for joining subpaths have to test against the BaseBackend instead of the BaseModel class.

--- txlib/api/tests/base_model.py
+++ txlib/api/tests/base_model.py
@@ -1,15 +1,19 @@
 # -*- coding: utf-8 -*-
 
 from txlib.utils.imports import unittest
-from txlib.api.base import BaseModel
+from txlib.api.base import BaseModel, BaseBackend
 
 
 class TestBaseModel(unittest.TestCase):
     """Test the base model for the Tx model wrappers."""
 
+
+class TestBaseBackend(unittest.TestCase):
+    """Test the base for the Tx model backends."""
+
     def test_join_subpaths(self):
         """Test that subpaths are joined correctly."""
-        b = BaseModel()
+        b = BaseBackend()
         correct_path = '/api/2/projects/'
 
         # all subpaths have slashes
