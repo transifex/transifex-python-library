31073a0d545efcd00db2ab94fcc998a61a593a4c
Apostolos Bessas <mpessas@indifex.com>
2011-09-21 17:36:34 +0300

    Add support for the source content to the Resource wrapper.
    The user can now save a resource along with the content, retrieve the source
    content and update it.

--- txlib/api/resources.py
+++ txlib/api/resources.py
@@ -13,6 +13,7 @@ class Resource(BaseModel):
 
     _path_to_collection = 'project/%(project_slug)s/resources/'
     _path_to_item = 'project/%(project_slug)s/resource/%(slug)s/?details'
+    _path_to_source_language = 'project/%(project_slug)s/resource/%(slug)s/content/'
 
     read_only_fields = set([
         'slug', 'name', 'created', 'available_languages', 'mimetype',
@@ -25,3 +26,29 @@ class Resource(BaseModel):
     ])
     mandatory_fields = set(['slug', 'name', 'mimetype', ])
     url_fields = set(['project_slug', 'slug', ])
+
+    def __getattr__(self, name):
+        """Make separate handling of the content of a resource."""
+        if name == 'content':
+            path = self._construct_path_to_source_content()
+            res = self._http.get(path)
+            self._populated_fields['content'] = res['content']
+            return res['content']
+        return super(Resource, self).__getattr__(name)
+
+    def _update(self, **kwargs):
+        """Use separate URL for updating the source file."""
+        if 'content' in kwargs:
+            content = kwargs.pop('content')
+            path = self._construct_path_to_source_content()
+            self._http.put(path, json.dumps({'content': content}))
+        super(Resource, self)._update(**kwargs)
+
+    def _construct_path_to_source_content(self):
+        """Construct the path to the source content for an actual resource."""
+        return self.path_to_source_content_template % self.url_parameters
+
+    @property
+    def path_to_source_content_template(self):
+        """Return the path to the source language content."""
+        return self._join_subpaths(self._prefix, self._path_to_source_language)
